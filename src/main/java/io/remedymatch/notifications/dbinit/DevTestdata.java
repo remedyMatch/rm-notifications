0721b2d98b4743d05f465324eb944fa878f15ff6
Matus Mala <matus.mala@gmail.com>
2020-05-03 14:31:06 +0200

    DevTestdata für Bedarf user

---
+++ src/main/java/io/remedymatch/notifications/dbinit/DevTestdata.java
@@ -0,0 +1,43 @@
+package io.remedymatch.notifications.dbinit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import io.remedymatch.notifications.infrastructure.jpa.BenachrichtigungEntity;
+import io.remedymatch.notifications.infrastructure.jpa.BenachrichtigungJpaRepository;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Profile("devtestdata")
+@Slf4j
+public class DevTestdata implements ApplicationListener<ContextRefreshedEvent> {
+
+	@Autowired
+	private BenachrichtigungJpaRepository benachrichtigungRepository;
+
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		legeTestNachrichtenAn();
+	}
+
+	@Transactional
+	public void legeTestNachrichtenAn() {
+		try {
+			benachrichtigungRepository.save(BenachrichtigungEntity.builder() //
+					.username("bedarf") //
+					.benachrichtigungKey("sample-benachrichtigung-ungelesen") //
+					.build());
+			benachrichtigungRepository.save(BenachrichtigungEntity.builder() //
+					.username("bedarf") //
+					.benachrichtigungKey("sample-benachrichtigung-gelesen") //
+					.build());
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			log.error("Fehler beim Erstellen des initialen Datenbestandes", ex);
+		}
+	}
+}
